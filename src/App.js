344acfa6db58c228599f7dc9e0e20f8ce25a9c6b
Homero Batista Kzam <homero@parkcode.mobi>
2019-03-22 11:25:39 -0300

    Adicionado o component Tab

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+import { Form, Template, Button } from 'devextreme-react';
+import { Item, SimpleItem, TabbedItem, Tab } from 'devextreme-react/form';
+
+const Endereco = (props) => {
+  return(
+    <>
+      Endereço
+    </>
+  )
+}
+
+const App = (props) => {
+  const handleOnClickSalvar = (e) => { alert("handleOnClickSalvar"); }
+  const handleOnClickCancelar = (e) => { alert("handleOnClickCancelar"); }
+
+  return (
+    <div className="App">
+      <Form>
+        <SimpleItem dataField="field1" />
+
+        <TabbedItem>
+          <Tab title="Pessoal">
+            <SimpleItem dataField="field2" />
+            <SimpleItem dataField="field3" />
+          </Tab>>
+          <Tab title="Trabalho">
+            <Item dataField="field4" />
+            <Item dataField="field5" />
+            <Item dataField="field6" />
+          </Tab>
+          <Tab title="Endereço" render={() => <Endereco />} />
+        </TabbedItem>
+
+        <Item template={'buttonsTemplate'} />
+        <Template name={'buttonsTemplate'}>
+          <Button text={'Salvar'} type={'success'} icon={'save'} onClick={handleOnClickSalvar} />
+          &nbsp;
+          <Button text={'Fechar'} type={'default'} icon={'close'} onClick={handleOnClickCancelar} />
+        </Template>
+
+      </Form>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import 'devextreme/dist/css/dx.common.css';
+import 'devextreme/dist/css/dx.light.compact.css';
+import 'devextreme/dist/css/dx.carmine.css';
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
