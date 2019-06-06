a33b06302f55dc85aa72c6b5d8830eab5a634ffd
Homero Batista Kzam <homero@parkcode.mobi>
2019-06-06 07:49:00 -0300

    Adicionado dxNumberBox

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 import { Form, Template, Button } from 'devextreme-react';
-import { Item, SimpleItem, TabbedItem, Tab } from 'devextreme-react/form';
+import { Item, GroupItem, SimpleItem, TabbedItem, Tab, ButtonItem } from 'devextreme-react/form';
 
 const Endereco = (props) => {
   return(
@@ -14,13 +14,35 @@ const Endereco = (props) => {
 }
 
 const App = (props) => {
+
+  const [currentData, setCurrentData] = useState({ data: new Date(), dinheiro: 1200.21, moeda: -12.25 });
+
   const handleOnClickSalvar = (e) => { alert("handleOnClickSalvar"); }
   const handleOnClickCancelar = (e) => { alert("handleOnClickCancelar"); }
 
   return (
     <div className="App">
-      <Form>
-        <SimpleItem dataField="field1" />
+      <Form formData={currentData}>
+        <GroupItem caption="Real">
+          <SimpleItem dataField={'data'} label={ {text: 'Data'}} editorType={'dxDateBox'} editorOptions={{ disabled: false }} />
+          <SimpleItem dataField={'dinheiro'} label={ {text: 'Cédula'}} editorType={'dxNumberBox'} editorOptions={{ format: { style: "currency", currency: "BRL" }, precision: 2, dataType: "number" }} />
+          <SimpleItem dataField={'moeda'} label={ {text: 'Gaveta'}} editorType={'dxNumberBox'} editorOptions={{ format: { style: "currency", currency: "BRL" }, precision: 2, dataType: "number" }} />
+        </GroupItem>
+
+        <GroupItem caption={'Personal Data'}>
+          <SimpleItem dataField={'Name'} />
+          <SimpleItem dataField={'Date'} editorType={'dxDateBox'} />
+        </GroupItem>
+        <GroupItem caption={'Billing address'}>
+          <SimpleItem dataField={'Country'} editorType={'dxSelectBox'} />
+          <SimpleItem dataField={'City'} editorType={'dxAutocomplete'} />
+          <SimpleItem dataField={'Address'} />
+          <SimpleItem dataField={'Phone'} helpText={'Enter the phone number in USA phone format'} />
+          <SimpleItem dataField={'Accepted'} editorType={'dxCheckBox'} />
+        </GroupItem>
+        <ButtonItem horizontalAlignment={'left'} buttonOptions={ {text: 'Register', type: 'success', useSubmitBehavior: true } } />
+
+        {/* <SimpleItem dataField="field1" />
 
         <TabbedItem>
           <Tab title="Pessoal">
@@ -40,7 +62,7 @@ const App = (props) => {
           <Button text={'Salvar'} type={'success'} icon={'save'} onClick={handleOnClickSalvar} />
           &nbsp;
           <Button text={'Fechar'} type={'default'} icon={'close'} onClick={handleOnClickCancelar} />
-        </Template>
+        </Template> */}
 
       </Form>
     </div>
--- src/index.js
+++ src/index.js
@@ -8,6 +8,13 @@ import 'devextreme/dist/css/dx.common.css';
 import 'devextreme/dist/css/dx.light.compact.css';
 import 'devextreme/dist/css/dx.carmine.css';
 
+import "devextreme-intl";
+import ptMessages from 'devextreme/localization/messages/pt.json';
+import { locale, loadMessages } from 'devextreme/localization';
+
+loadMessages(ptMessages);
+locale("pt-BR");
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
